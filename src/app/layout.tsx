0dcbfe9475525aee683ca9ac4d1eae16f9d414f5
ctravizanutto <ctravizanutto@gmail.com>
2025-04-06 23:14:31 -0300

    feat: vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
+        <SpeedInsights />
         {children}
         <Toaster />
       </body>
